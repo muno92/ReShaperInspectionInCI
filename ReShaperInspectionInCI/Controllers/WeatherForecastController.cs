37084115988ee6d01d7af8fdb65d96348d09ffda
n-ogawa <fy59.kou-729@live.jp>
2021-04-25 17:56:51 +0900

    fix inspection issues

--- ReShaperInspectionInCI/Controllers/WeatherForecastController.cs
+++ ReShaperInspectionInCI/Controllers/WeatherForecastController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,7 +11,6 @@ namespace ReShaperInspectionInCI.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            new Random();
             return Enumerable.Range(1, 5).Select(_ => new WeatherForecast())
                 .ToArray();
         }
