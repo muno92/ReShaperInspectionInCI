6d13dd663b61d3d4e33209aaa29d49d542c4231f
n-ogawa <fy59.kou-729@live.jp>
2021-04-25 17:43:18 +0900

    fix inspection issues

--- ReShaperInspectionInCI/Controllers/WeatherForecastController.cs
+++ ReShaperInspectionInCI/Controllers/WeatherForecastController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace ReShaperInspectionInCI.Controllers
 {
@@ -11,23 +9,10 @@ namespace ReShaperInspectionInCI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private int UnusedField;
-
-        public string TooOpenField;
-        
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-            TooOpenField = "this is field should be private.";
-            var unusedVariable = "test";
-        }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
