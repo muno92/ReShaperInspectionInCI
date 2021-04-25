d91bc7c900cdc76276cb2bd8da9938a210760d4c
n-ogawa <fy59.kou-729@live.jp>
2021-04-25 17:49:42 +0900

    fix inspection issues

--- ReShaperInspectionInCI/Startup.cs
+++ ReShaperInspectionInCI/Startup.cs
@@ -8,12 +8,12 @@ namespace ReShaperInspectionInCI
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
-
-        private IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
